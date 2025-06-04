e37020b6677f20731e7f2ac9e3343bbde5b31683
heywowyou <pro@julesfontaine.com>
2025-06-04 17:07:13 +0200

    Align navigation to end of the header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,17 +3,17 @@ import Link from "next/link";
 export default function Header() {
   return (
     <header>
-      <div className="flex min-w-full px-30 py-6">
+      <div className="flex min-w-full px-30 py-10">
         <div className="flex flex-auto">
           <Link href="/" className="text-4xl font-bold">
             JULES FONTAINE
           </Link>
         </div>
-        <div className="flex flex-auto gap-20">
-          <Link href="/" className="text-2xl">
+        <div className="flex flex-auto justify-end gap-10">
+          <Link href="/home" className="text-2xl">
             Home
           </Link>
-          <Link href="/" className="text-2xl">
+          <Link href="/work" className="text-2xl">
             Work
           </Link>
         </div>
