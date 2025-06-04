3e6bc4da8b175bbf0c5bfa3a4ee51f021df5a8e6
heywowyou <pro@julesfontaine.com>
2025-06-04 14:57:47 +0200

    Update Header and remove temp the dark mode colors

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,23 @@
+import Link from "next/link";
+
 export default function Header() {
   return (
     <header>
-      <div></div>
+      <div className="flex min-w-full px-30 py-6">
+        <div className="flex flex-auto">
+          <Link href="/" className="text-4xl font-bold">
+            JULES FONTAINE
+          </Link>
+        </div>
+        <div className="flex flex-auto gap-20">
+          <Link href="/" className="text-2xl">
+            Home
+          </Link>
+          <Link href="/" className="text-2xl">
+            Work
+          </Link>
+        </div>
+      </div>
     </header>
   );
 }
